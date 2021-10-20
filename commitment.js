639906d58f1028f7debf379e15bab4e0934a70de
windowdotonload <275981471@qq.com>
2021-10-20 23:39:39 +0800

    mod

--- commitment.js
+++ commitment.js
@@ -3,4 +3,24 @@
  * @version:
  * @Author: windowdotonload
  */
-class Commitment {}
+class Commitment {
+  static PENDING = "PENDING";
+  static FULFILL = "FULFILL";
+  static REJECT = "REJECT";
+  status = Commitment.PENDING;
+  constructor(func) {
+    func(this.resolve.bind(this), this.reject.bind(this));
+  }
+
+  resolve() {
+    console.log(this.status);
+  }
+
+  reject() {}
+
+  then() {}
+}
+
+let com = new Commitment((res, rej) => {
+  res("123");
+});
