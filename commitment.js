5d576d9cf81befa59a9322227218bbe662c11f33
windowdotonload <275981471@qq.com>
2021-10-21 19:20:10 +0800

    mod

--- commitment.js
+++ commitment.js
@@ -5,20 +5,60 @@
  */
 class Commitment {
   static PENDING = "PENDING";
-  static FULFILL = "FULFILL";
-  static REJECT = "REJECT";
+  static FULFILLED = "FULFILL";
+  static REJECTED = "REJECT";
   status = Commitment.PENDING;
+  resolveCallbacks = [];
+  rejectCallbacks = [];
+  result = null;
   constructor(func) {
-    func(this.resolve.bind(this), this.reject.bind(this));
+    try {
+      func(this.resolve.bind(this), this.reject.bind(this));
+    } catch (e) {
+      this.reject(e);
+    }
   }
 
-  resolve() {
-    console.log(this.status);
+  resolve(res) {
+    setTimeout(() => {
+      if (this.status == Commitment.PENDING) {
+        this.status = Commitment.FULFILL;
+        this.result = res;
+        this.resolveCallbacks.forEach((callback) => callback(res));
+      }
+    });
   }
 
-  reject() {}
+  reject(res) {
+    setTimeout(() => {
+      if (this.status == Commitment.REJECTED) {
+        this.status = Commitment.REJECTED;
+        this.result = res;
+        this.resolveCallbacks.forEach((callback) => callback(res));
+      }
+    });
+  }
 
-  then() {}
+  then(onFULFULLED, onREJECTED) {
+    return new Commitment((res, rej) => {
+      onFULFULLED = typeof onFULFULLED == "function" ? onFULFULLED : () => {};
+      onREJECTED = typeof onREJECTED == "function" ? onREJECTED : () => {};
+      if (this.status == Commitment.PENDING) {
+        this.resolveCallbacks.push(onFULFULLED);
+        this.rejectCallbacks.push(onREJECTED);
+      }
+      if (this.status == Commitment.FULFILLED) {
+        setTimeout(() => {
+          onFULFULLED(this.result);
+        });
+      }
+      if (this.status == Commitment.REJECTED) {
+        setTimeout(() => {
+          onREJECTED(this.result);
+        });
+      }
+    });
+  }
 }
 
 let com = new Commitment((res, rej) => {
